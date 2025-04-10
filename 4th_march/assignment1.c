620531c529f8d2cd1b064b16f4e73a3e95ee7e23
santosh methre <santoshmethre22@gmail.com>
2025-04-10 18:02:58 +0530

    file info is beging add

--- 4th_march/assignment1.c
+++ 4th_march/assignment1.c
@@ -6,14 +6,18 @@
 
 #define META_SIZE 4096  // First 4096 bytes reserved for metadata
 
-typedef struct {
+typedef struct Super {
     int n;      // total number of blocks
     int s;      // size of each block
     int ubn;    // number of used blocks
     int fbn;    // number of free blocks
     // followed by bitmap (ub), each bit represents if a block is used
-} FileMeta;
+} file_info;
 
+
+typedef struct File{
+    char content[2048]; 
+}file;
 // Function to write metadata and bitmap to file
 int write_meta(int fd, FileMeta *meta, unsigned char *bitmap) {
     lseek(fd, 0, SEEK_SET);
@@ -32,34 +36,40 @@ int read_meta(int fd, FileMeta *meta, unsigned char *bitmap) {
     return 0;
 }
 
+
+//Write a function "int init_File_dd(const char *fname, int bsize, int bno)" that creates a file (if it does not exist) called fname of appropriate size (4096 + bsize*bno Bytes) in your folder. The function initializes the first 4096 Byes 
+// putting proper values for n, s, ubn, fbn, and ub. If for some reason this functions fails, it returns -1. Otherwise it returns 0.
+
+
 int init_File_dd(const char *fname, int bsize, int bno) {
-    int fd = open(fname, O_RDWR | O_CREAT, 0666);
-    if (fd < 0) return -1;
+   // int fd=open(fname,)
+     int fd = open(fname, O_RDWR | O_CREAT, 0666);
+     if (fd < 0) return -1;
 
-    int bitmap_size = (bno + 7) / 8;
-    unsigned char *bitmap = calloc(1, bitmap_size);
-    if (!bitmap) return -1;
+    // int bitmap_size = (bno + 7) / 8;
+    // unsigned char *bitmap = calloc(1, bitmap_size);
+    // if (!bitmap) return -1;
 
-    FileMeta meta = {
-        .n = bno,
-        .s = bsize,
-        .ubn = 0,
-        .fbn = bno
-    };
+    file_info.n=bno;
+    file_info.s=bsize;
+    file_info.ubn=0;
+    file_info.fbn=bno;
 
-    if (write_meta(fd, &meta, bitmap) < 0) {
-        free(bitmap);
-        close(fd);
-        return -1;
-    }
 
-    // Expand the file to full size (META_SIZE + bsize * bno)
-    lseek(fd, META_SIZE + bsize * bno - 1, SEEK_SET);
-    write(fd, "", 1);
 
-    free(bitmap);
-    close(fd);
-    return 0;
+    // if (write_meta(fd, &meta, bitmap) < 0) {
+    //     free(bitmap);
+    //     close(fd);
+    //     return -1;
+    // }
+
+    // // Expand the file to full size (META_SIZE + bsize * bno)
+    // lseek(fd, META_SIZE + bsize * bno - 1, SEEK_SET);
+    // write(fd, "", 1);
+
+    // free(bitmap);
+    // close(fd);
+    // return 0;
 }
 
 int get_freeblock(const char *fname) {
