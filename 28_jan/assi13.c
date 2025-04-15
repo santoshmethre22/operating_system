e9e5e385e13869e6d1f7a0be453f4f8e98a4ce0e
santoshmethre22 <santoshmethre22@gmail.com>
2025-04-15 15:34:43 +0530

    the modified assi13.c

--- 28_jan/assi13.c
+++ 28_jan/assi13.c
@@ -1,15 +1,16 @@
-// forkNexec1.c
+// forkNexec3.c
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
- #include <unistd.h>
-
- #include <sys/wait.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 
+extern char **environ;
 
 int main() {
     char input[256];
+
     while (1) {
         printf("Enter an executable name: ");
         if (fgets(input, sizeof(input), stdin) == NULL) {
@@ -17,27 +18,38 @@ int main() {
             break;
         }
 
-        // Remove newline character from input, if any
-        // remove new other way
+        // Remove newline character from input
+        input[strcspn(input, "\n")] = '\0';
 
-        pid_t pid=fork();
+        pid_t pid = fork();
 
-        if(pid==0){
+        if (pid == 0) {
+            // Child process
+            printf("You entered the file name -----> : %s\n", input);
 
-            input[strcspn(input, "\n")] = 0;
+            char *myargv[] = {input, NULL};
+            execve(input, myargv, environ);
 
-            printf("you entered the file name -----> : %s\n ",input);
-            
-            char *myargv[]={input,NULL};
-            
-            execve(input,myargv,NULL);
-
+            // If execve fails, print reason and exit
+            perror("execve failed");
+            exit(EXIT_FAILURE);
         }
 
-        wait(NULL);
+        // Parent process
+        int status;
+        waitpid(pid, &status, 0);
 
+        printf("Child process (PID %d) terminated. ", pid);
 
-        //printf("You entered: %s\n", input);
+        if (WIFEXITED(status)) {
+            printf("Exited normally with status %d.\n", WEXITSTATUS(status));
+        } else if (WIFSIGNALED(status)) {
+            printf("Terminated by signal %d (%s).\n", WTERMSIG(status), strsignal(WTERMSIG(status)));
+        } else if (WIFSTOPPED(status)) {
+            printf("Stopped by signal %d.\n", WSTOPSIG(status));
+        } else {
+            printf("Unknown termination.\n");
+        }
     }
 
     return 0;
